72c128414e1b899d03c1f8d8fa3806151d53f82e
Vijay.Kum.Verma <cerma.vijay22@gmail.com>
2025-07-14 09:53:12 +0530

    1st commit

--- public/electron.js
+++ public/electron.js
@@ -6,14 +6,21 @@ function createWindow() {
         width: 1280,
         height: 800,
         webPreferences: {
-            nodeIntegration: true,
+            contextIsolation: false, // Allow interaction but isolate context
+            nodeIntegration: true,   // Enable Node.js APIs in renderer
         },
     });
 
     win.loadFile(path.join(__dirname, 'build', 'index.html'));
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
